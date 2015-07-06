d27fe838fd8a0d9dc73ef19939b09e3d5840dd25
ayasuda2 <shinsuke.katsuyama@gmail.com>
2015-07-06 18:47:53 +0900

    my challenge commit 1
    ちょっとチャレンジングなコミット

--- what_is_git.rb
+++ what_is_git.rb
@@ -1,6 +1,13 @@
 class WhatIsGit
   def show
-    puts "Git is easy!"
+    puts "Do you understand the basis of git? [yes/no]"
+    input = gets.chomp.downcase
+    case input
+    when "yes", "y"
+      puts "Git is easy!"
+    else
+      puts "Git is difficult..."
+    end
   end
 
   def about
